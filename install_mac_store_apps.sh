e59cf30d64780fa6d4ccde5815bef30401b1dba1
Matt Woods <mw@mattwoods.io>
2020-10-04 16:25:44 -0500

    Add MAS install script

---
+++ install_mac_store_apps.sh
@@ -0,0 +1,16 @@
+# Amphetamine
+mas install 937984704
+
+# Magnet
+mas install 441258766
+
+# Xcode
+mas install 497799835
+
+# Pocket
+mas install 568494494
+
+# Reeder
+mas install 1449412482
+
+echo "all done"
